45090056192ec819452965da2d561fc702267367
abh224272 <abh224272@gmail.com>
2020-11-23 20:11:09 +0530

    Added: Regex to check Valid First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
 echo "=>=>=>=>=>=>=>=>Welcome to User Registration<=<=<=<=<=<=<=<="
+
+echo "Enter the First name which have first letter as capital and minimum 3 letters "
+read firstName
+firstNameRegex="^[A-Z][a-z]{2,}$"
+
+if [[ $firstName =~ $firstNameRegex ]]
+then
+	echo "Valid First Name"
+else
+	echo "Invalid First Name....Enter it according to rule"
+fi
