762ad82e1faabb70903d61af4e83fdaa9f0328aa
abh224272 <abh224272@gmail.com>
2020-11-23 20:37:05 +0530

    Added: Regex to check valid Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -12,3 +12,21 @@ then
 else
 	echo "Invalid First Name....Enter it according to rule"
 fi
+
+echo "********************"
+
+
+echo "Enter the Last Name which have first letter as capital and minimum 3 letters "
+read lastName
+lastNameRegex="^[A-Z][a-z]{2,}$"
+
+if [[ $lastName =~ $lastNameRegex ]]
+then
+        echo "Valid Last Name"
+else
+        echo "Invalid Last Name....Enter it according to rule"
+fi
+
+
+
+
